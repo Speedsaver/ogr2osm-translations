6245b196df811bf65fbd272d21f3a8dd3f10e566
Xogium <contact@xogium.me>
2021-06-18 23:53:10 +0200

    convert.sh: now expects a maptool binary to be copied in the translations directory.

--- convert.sh
+++ convert.sh
@@ -6,7 +6,7 @@ python3 ../ogr2osm.py -t speedsaver --positive-id --force -o convert.osm "$1"
 echo "#### LINTING MAP ####"
 xmllint --pretty 1 convert.osm > linted.osm
 echo "#### RUNNING MAPTOOL ####"
-maptool -U out.bin < linted.osm
+./maptool -U out.bin < linted.osm
 echo "#### CLEANING UP ####"
 rm convert.osm
 rm linted.osm
